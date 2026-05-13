08c55e62eb9887c8ef25325b7beb13d5e3c5a1f8
wangjh2024 <363319973@qq.com>
2026-05-13 12:01:38 +0800

    ignore

--- install/cube_imu_calibration/share/cube_imu_calibration/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(cube_imu_calibration_IDL_FILES "msg/CubePose.idl;msg/CubePoseStatus.idl;msg/RecorderStatus.idl")
-set(cube_imu_calibration_INTERFACE_FILES "msg/CubePose.msg;msg/CubePoseStatus.msg;msg/RecorderStatus.msg")
